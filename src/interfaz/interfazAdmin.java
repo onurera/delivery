f7299c943027f0b68b1782feece45723b0da274d
Unknown <lucianojuarezgaston@gmail.com>
2017-09-16 00:26:37 -0300

    nuevo Funciona alta Cadete

--- src/interfaz/interfazAdmin.java
+++ src/interfaz/interfazAdmin.java
@@ -32,42 +32,34 @@ public class interfazAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        jButton1AgregarEmpleado = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Empleados");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton1AgregarEmpleado.setText("Empleados");
+        jButton1AgregarEmpleado.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton1AgregarEmpleadoActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1AgregarEmpleado, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 60, 136, 38));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(54, 54, 54)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(210, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(108, 108, 108)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(154, Short.MAX_VALUE))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fondoNaranja.jpg"))); // NOI18N
+        jLabel1.setMaximumSize(new java.awt.Dimension(255, 255));
+        jLabel1.setMinimumSize(new java.awt.Dimension(255, 255));
+        jLabel1.setPreferredSize(new java.awt.Dimension(600, 600));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 340, 330));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1AgregarEmpleadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1AgregarEmpleadoActionPerformed
        JFrame ventanaEmpleados = new interfazUsuarioAdmi();
        ventanaEmpleados.setVisible(true);
        setVisible(false);        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton1AgregarEmpleadoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -105,6 +97,7 @@ public class interfazAdmin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton1AgregarEmpleado;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
